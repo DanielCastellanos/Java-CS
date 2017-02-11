7e7cde4887d1cdd3fd33662d9963182c8844b561
DanielCastellanos <castellanos_daniel@outlook.com>
2017-02-11 00:04:04 -0600

    Add files via upload

---
+++ Cliente/src/bloqueo/CheckConnection.java
@@ -0,0 +1,50 @@
+package bloqueo;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.Timer;
+
+public class CheckConnection {
+
+    public boolean isConnected() {
+        try {
+            WebClient webClient = new WebClient();
+
+            HtmlPage page1 = webClient.getPage("http://siiauescolar.siiau.udg.mx/wus/gupprincipal.forma_inicio");
+            HtmlForm form = page1.getFormByName("detect");
+//nombre de la caja de texto 
+            HtmlTextInput codigo = form.getInputByName("p_codigo_c");
+            HtmlPasswordInput clave = form.getInputByName("p_clave_c");
+//nombre del boton del formulario final 
+            HtmlSubmitInput button = form.getInputByValue("Ingresar");
+//llenamos la caja de texto 
+            codigo.setValueAttribute("123456789");
+            clave.setValueAttribute("23654789");
+//Creamos la pagina que nos devolverá el resultado final 
+            HtmlPage pageResultado;
+//hacemos clic en el boton del formulario y asignamos el resultado a la pagina pageResultado 
+            pageResultado = button.click();
+
+            webClient.close();
+            return true;
+        } catch (IOException ex) {
+
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        } catch (FailingHttpStatusCodeException ex) {
+
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+}
---
+++ Cliente/src/bloqueo/Login.java
@@ -0,0 +1,55 @@
+package bloqueo;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Login {
+
+    public boolean login(String cod, String nip) {
+        //emulamos un navegador web final 
+        boolean log = false;
+
+        try {
+            WebClient webClient = new WebClient();
+//Pagina donde se hara la consulta 
+            HtmlPage page1 = webClient.getPage("http://siiauescolar.siiau.udg.mx/wus/gupprincipal.forma_inicio");
+//nombre del formulario final 
+            HtmlForm form = page1.getFormByName("detect");
+//el valor "detect" no es arbitrario es el nombre del formulario web de siiau 
+//nombre de la caja de texto 
+            HtmlTextInput codigo = form.getInputByName("p_codigo_c");
+            HtmlPasswordInput clave = form.getInputByName("p_clave_c");
+//el valor "p_codigo_c" no es arbitrario es el nombre de la caja de texto del formulario web de siiau 
+//nombre del boton del formulario final 
+            HtmlSubmitInput button = form.getInputByValue("Ingresar");
+//el valor "Ingresar" no es arbitrario es el nombre del boton del formulario web de google 
+//llenamos la caja de texto 
+            codigo.setValueAttribute(cod);
+            clave.setValueAttribute(nip);
+//Creamos la pagina que nos devolverá el resultado final 
+            HtmlPage pageResultado;
+//hacemos clic en el boton del formulario y asignamos el resultado a la pagina pageResultado 
+            pageResultado = button.click();
+//imprimimos el resultado 
+            String res = pageResultado.asText();
+            if (res.equals("Untitled Document")) {
+                log = true;
+            }
+//cerramos el navegador emulado, para liberar todo esto de la memoria 
+            webClient.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (FailingHttpStatusCodeException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return log;
+    }
+}
